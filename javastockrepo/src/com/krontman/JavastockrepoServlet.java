90008e24a3ab2c2c93ea4369525013044cdd51da
marga <marga@10.0.0.3>
2015-04-24 02:40:30 +0300

    EX 02

--- javastockrepo/src/com/krontman/JavastockrepoServlet.java
+++ javastockrepo/src/com/krontman/JavastockrepoServlet.java
@@ -7,7 +7,15 @@ import javax.servlet.http.*;
 public class JavastockrepoServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1;
+		int num2;
+		int num3;
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+		int result = num3 * (num1 + num2);
+		String resultStr = new String( "<h1>Result of "+num3+"*("+num1+ "+"+num2+")="+result+"</h1>");
+		resp.getWriter().println(resultStr);
 	}
 }
